ba536003de48379672f3d861419d3471d6795f67
user <frostkslo1@yandex.ru>
2022-09-16 18:25:49 +0400

    что

---
+++ app/src/main/java/com/example/volga_it/ChartScreen.kt
@@ -0,0 +1,11 @@
+package com.example.volga_it
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart_screen)
+    }
+}
